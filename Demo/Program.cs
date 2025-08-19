b8054c24d59fad6cff9a3a58b664cdd456b9aa74
Andreas <Andreas.Zwander@gmail.com>
2025-08-20 01:27:28 +0200

    Add Demo Code

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using NetRangeManager.Models; // Wichtig: Hier binden wir unsere NetRangeV4-Klasse ein!
 
-Console.WriteLine ( "Hello, World!" );
+Console.WriteLine("--- NetRangeManager Demo ---");
+Console.WriteLine();
+
+// 1. Erstellen wir einen neuen IP-Bereich
+var network = new NetRangeV4("192.168.1.0/24");
+
+Console.WriteLine($"Netzwerk erstellt: {network.NetworkAddress}/{network.CidrPrefix}");
+Console.WriteLine($"Broadcast-Adresse: {network.LastAddressInRange}");
+Console.WriteLine($"Erste nutzbare IP: {network.FirstUsableAddress}");
+Console.WriteLine($"Anzahl Adressen: {network.TotalAddresses}");
+Console.WriteLine("------------------------------------");
+Console.WriteLine();
+
+// 2. Testen wir die 'Contains'-Methode
+Console.WriteLine("Teste IPs mit der Contains()-Methode:");
+
+var ipInside = IPAddress.Parse("192.168.1.150");
+var ipOutside = IPAddress.Parse("10.0.0.5");
+var ipIsNetworkAddress = network.NetworkAddress;
+var ipIsBroadcastAddress = network.LastAddressInRange;
+
+Console.WriteLine($"Liegt {ipInside} im Bereich? ---> {network.Contains(ipInside)}"); // Erwartet: True
+Console.WriteLine($"Liegt {ipOutside} im Bereich? ---> {network.Contains(ipOutside)}"); // Erwartet: False
+Console.WriteLine($"Liegt {ipIsNetworkAddress} im Bereich? ---> {network.Contains(ipIsNetworkAddress)}"); // Erwartet: True
+Console.WriteLine($"Liegt {ipIsBroadcastAddress} im Bereich? ---> {network.Contains(ipIsBroadcastAddress)}"); // Erwartet: True
+
+Console.WriteLine();
+Console.WriteLine("--- Demo Ende ---");
