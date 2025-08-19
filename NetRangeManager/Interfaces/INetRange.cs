5b891694e72c96faf60c03b060c9850a9b828bfa
Andreas <Andreas.Zwander@gmail.com>
2025-08-20 00:50:22 +0200

    Initial commit

---
+++ NetRangeManager/Interfaces/INetRange.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Numerics;
+
+namespace NetRangeManager;
+
+/// <summary>
+/// Definiert einen allgemeinen Vertrag für einen IP-Adressbereich (sowohl IPv4 als auch IPv6).
+/// </summary>
+/// <typeparam name="TNetRange">Der konkrete Typ des IP-Bereichs, der dieses Interface implementiert.</typeparam>
+public interface INetRange<TNetRange> : IComparable<TNetRange>, IEquatable<TNetRange> 
+    where TNetRange : INetRange<TNetRange>
+{
+    // --- Eigenschaften ---
+    IPAddress NetworkAddress { get; }
+    int CidrPrefix { get; }
+    IPAddress FirstUsableAddress { get; }
+    IPAddress LastAddressInRange { get; }
+    BigInteger TotalAddresses { get; }
+    bool IsHost { get; }
+
+    // --- Methoden ---
+    bool Contains(IPAddress ipAddress);
+    bool OverlapsWith(TNetRange other);
+    bool IsSubnetOf(TNetRange other);
+    bool IsSupernetOf(TNetRange other);
+
+    /// <summary>
+    /// Teilt das aktuelle Netzwerk in kleinere Subnetze mit dem angegebenen neuen Präfix auf.
+    /// </summary>
+    /// <param name="newPrefix">Das neue CIDR-Präfix für die Subnetze. Muss größer als das aktuelle sein.</param>
+    /// <returns>Eine Aufzählung der resultierenden Subnetze.</returns>
+    IEnumerable<TNetRange> GetSubnets(int newPrefix);
+}
---
+++ NetRangeManager/Models/NetRangeV4.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Net;
+
+namespace NetRangeManager.Models;
+
+public readonly record struct NetRangeV4 : INetRange< NetRangeV4 >
+{
+    public bool Equals ( NetRangeV4 other ) { throw new NotImplementedException(); }
+    public int CompareTo ( NetRangeV4 other ) { throw new NotImplementedException(); }
+    public IPAddress NetworkAddress { get; }
+    public int CidrPrefix { get; }
+    public IPAddress FirstUsableAddress { get; }
+    public IPAddress LastAddressInRange { get; }
+    public BigInteger TotalAddresses { get; }
+    public bool IsHost { get; }
+    public bool Contains ( IPAddress ipAddress ) { throw new NotImplementedException(); }
+    public bool OverlapsWith ( NetRangeV4 other ) { throw new NotImplementedException(); }
+    public bool IsSubnetOf ( NetRangeV4 other ) { throw new NotImplementedException(); }
+    public bool IsSupernetOf ( NetRangeV4 other ) => other.IsSubnetOf ( this );
+    public IEnumerable< NetRangeV4 > GetSubnets ( int newPrefix ) { throw new NotImplementedException(); }
+}
