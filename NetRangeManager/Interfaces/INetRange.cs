03d01e2af4e310d54726db38da17be34f03d7f3a
Andreas <Andreas.Zwander@gmail.com>
2025-08-20 02:59:10 +0200

    update

--- NetRangeManager/Interfaces/INetRange.cs
+++ NetRangeManager/Interfaces/INetRange.cs
@@ -1,5 +1,5 @@
-﻿using System.Net;
-using System.Numerics;
+﻿using System.Numerics;
+using System.Net;
 
 namespace NetRangeManager.Interfaces;
 
@@ -7,22 +7,70 @@ namespace NetRangeManager.Interfaces;
 /// Definiert einen allgemeinen Vertrag für einen IP-Adressbereich (sowohl IPv4 als auch IPv6).
 /// </summary>
 /// <typeparam name="TNetRange">Der konkrete Typ des IP-Bereichs, der dieses Interface implementiert.</typeparam>
-public interface INetRange<TNetRange> : IComparable<TNetRange>, IEquatable<TNetRange>
+public interface INetRange<TNetRange> : IComparable<TNetRange>, IEquatable<TNetRange> 
     where TNetRange : INetRange<TNetRange>
 {
-    // --- Eigenschaften ---
+    /// <summary>
+    /// Ruft die erste Adresse des Netzwerkbereichs ab (Netzwerkadresse).
+    /// </summary>
     IPAddress NetworkAddress { get; }
+
+    /// <summary>
+    /// Ruft das CIDR-Präfix ab, das die Größe des Netzwerks bestimmt (z.B. 24 für /24).
+    /// </summary>
     int CidrPrefix { get; }
+
+    /// <summary>
+    /// Ruft die erste nutzbare Host-Adresse im Bereich ab.
+    /// </summary>
     IPAddress FirstUsableAddress { get; }
-    IPAddress LastUsableAddress { get; } // <-- NEUE ZEILE
+
+    /// <summary>
+    /// Ruft die letzte nutzbare Host-Adresse im Bereich ab.
+    /// </summary>
+    IPAddress LastUsableAddress { get; }
+
+    /// <summary>
+    /// Ruft die absolut letzte Adresse im Bereich ab (Broadcast-Adresse bei IPv4).
+    /// </summary>
     IPAddress LastAddressInRange { get; }
+
+    /// <summary>
+    /// Ruft die Gesamtzahl der IP-Adressen im Bereich ab.
+    /// </summary>
     BigInteger TotalAddresses { get; }
+
+    /// <summary>
+    /// Gibt an, ob der Bereich nur eine einzelne Host-Adresse repräsentiert (/32 bei IPv4, /128 bei IPv6).
+    /// </summary>
     bool IsHost { get; }
 
-    // --- Methoden ---
+    /// <summary>
+    /// Überprüft, ob die angegebene IP-Adresse in diesem Netzwerkbereich enthalten ist.
+    /// </summary>
+    /// <param name="ipAddress">Die zu überprüfende IP-Adresse.</param>
+    /// <returns><c>true</c>, wenn die Adresse im Bereich liegt, andernfalls <c>false</c>.</returns>
     bool Contains(IPAddress ipAddress);
+
+    /// <summary>
+    /// Überprüft, ob dieser Netzwerkbereich mit einem anderen Bereich überlappt.
+    /// </summary>
+    /// <param name="other">Der andere zu vergleichende Netzwerkbereich.</param>
+    /// <returns><c>true</c>, wenn eine Überlappung besteht, andernfalls <c>false</c>.</returns>
     bool OverlapsWith(TNetRange other);
+
+    /// <summary>
+    /// Überprüft, ob dieser Bereich ein Subnetz des anderen angegebenen Bereichs ist.
+    /// </summary>
+    /// <param name="other">Der potenzielle Supernet-Bereich.</param>
+    /// <returns><c>true</c>, wenn dieser Bereich vollständig im anderen enthalten ist.</returns>
     bool IsSubnetOf(TNetRange other);
+
+    /// <summary>
+    /// Überprüft, ob dieser Bereich ein Supernet des anderen angegebenen Bereichs ist.
+    /// </summary>
+    /// <param name="other">Der potenzielle Subnet-Bereich.</param>
+    /// <returns><c>true</c>, wenn der andere Bereich vollständig in diesem enthalten ist.</returns>
     bool IsSupernetOf(TNetRange other);
 
     /// <summary>
