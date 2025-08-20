cddcb13fa614da42d2170bdb36648484036a8c14
Andreas <Andreas.Zwander@gmail.com>
2025-08-20 02:05:28 +0200

    V6

---
+++ NetRangeManager/Models/NetRangeV6.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using System.Net;
+using NetRangeManager.Interfaces;
+
+namespace NetRangeManager.Models;
+
+public readonly partial record struct NetRangeV6 : INetRange<NetRangeV6>
+{
+    public bool Equals(NetRangeV6 other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(NetRangeV6 other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IPAddress NetworkAddress { get; }
+    public int CidrPrefix { get; }
+    public IPAddress FirstUsableAddress { get; }
+    public IPAddress LastUsableAddress { get; }
+    public IPAddress LastAddressInRange { get; }
+    public BigInteger TotalAddresses { get; }
+    public bool IsHost { get; }
+
+    public bool Contains(IPAddress ipAddress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool OverlapsWith(NetRangeV6 other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSubnetOf(NetRangeV6 other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSupernetOf(NetRangeV6 other) => other.IsSubnetOf(this);
+    
+    public IEnumerable<NetRangeV6> GetSubnets(int newPrefix)
+    {
+        throw new NotImplementedException();
+    }
+}
